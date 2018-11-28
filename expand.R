5d53a20d65b3bbe187e77cdd3fb9a3244eec1b7e
Ryan Patrick Kyle <ryan@plot.ly>
2018-11-27 21:36:07 -0500

    Removed misplaced files

--- expand.R
+++
@@ -1,8 +0,0 @@
-expand <-
-function(dataset, size) {
-	size <- abs(size)
-	if(!(all(floor(size) == size, na.rm = TRUE) & length(size) == 1)) {
-		stop("argument size must be an integer of length 1!")
-	} # verify value is an integer of length one
-	return(dataset[rep(1:nrow(dataset), each=size),])
-}
